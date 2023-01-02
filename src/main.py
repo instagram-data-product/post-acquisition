6aaff0ddf2ad453588fa657f7a8fad600fa340e1
jsnaz <jsnazareno1991@gmail.com>
2023-01-02 19:47:02 +0100

    Integrate export images to bucket in the get_html_from_profile_url function

--- src/main.py
+++ src/main.py
@@ -2,31 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-from init_utils import accept_cookies, login, save_login_info, turn_off_notifications, search_profile, find_and_collect_posts
+from init_utils import accept_cookies, login, save_login_info, turn_off_notifications, search_profile, \
+    find_and_collect_posts, upload_from_directory
+
+LOCAL_IMAGES_FOLDER = "Images"
+GCS_BUCKET_NAME = "instagram_scrapping_bucket"
+BLOB_NAME = "posts"
 
 
-def export_html(driver):
-    html = driver.page_source
-    file = open("instagram.html", "w", encoding="utf-8")
-    file.write(html)
-    file.close()
-
-
-def get_html_from_profile_url():
+def get_html_from_profile_url(username):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.instagram.com/")
-
 
     accept_cookies(driver)
     login(driver)
     save_login_info(driver)
     turn_off_notifications(driver)
-    search_profile(driver)
+    search_profile(driver, username)
     find_and_collect_posts(driver)
-    export_html(driver)
-
+    upload_from_directory(LOCAL_IMAGES_FOLDER, GCS_BUCKET_NAME, BLOB_NAME, username)
     # Go to a user page
     # Get all the images (handle the scrolling)
 
 
-get_html_from_profile_url()
+keywords_list = [
+    "ragdoll_brotherz",
+    "lonepsi"
+]
+
+for keywords in keywords_list:
+    print("Data Acquisition for " + keywords)
+    get_html_from_profile_url(keywords)
